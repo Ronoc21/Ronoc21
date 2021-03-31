a8e61185558bf26acf5cbaad90350f1606706ac7
Conor <conor.okeefe.2121@gmail.com>
2021-03-30 21:03:36 -0700

    animal.py

---
+++ animal.py
@@ -0,0 +1,7 @@
+# Author: Conor Smith
+# Date: 03/30/2021
+# Description: Asks the user for their favorite animal and then
+#              prints out that that is their favorite animal
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
